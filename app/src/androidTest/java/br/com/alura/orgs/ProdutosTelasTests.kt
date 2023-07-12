ab7069b09c00e367b6a341c02a12a63624d9729f
Lucas Almeida <lucas.spereira@stone.com.br>
2023-07-12 13:09:14 -0300

    Curso de Android com kotlin: Testes de unidade e instrumentados

---
+++ app/src/androidTest/java/br/com/alura/orgs/ProdutosTelasTests.kt
@@ -0,0 +1,131 @@
+package br.com.alura.orgs
+
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import br.com.alura.orgs.database.AppDatabase
+import br.com.alura.orgs.ui.activity.FormularioProdutoActivity
+import br.com.alura.orgs.ui.activity.ListaProdutosActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ProdutosTelasTests {
+
+    @get:Rule
+    val rule = ActivityScenarioRule(ListaProdutosActivity::class.java)
+
+    @Before
+    fun preparaAmbiente() {
+        AppDatabase.instancia(
+            InstrumentationRegistry.getInstrumentation().targetContext
+        ).clearAllTables()
+    }
+
+    @Test
+    fun deveMostrarONomeDoAplicativo() {
+        //Assert
+        onView(withText("Orgs")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun deveMostrarCamposNecessarioParaCriarUmProduto() {
+        //act
+        clicaNoFAB()
+
+        //Assert
+        onView(withId(R.id.activity_formulario_produto_nome)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_formulario_produto_descricao)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_formulario_produto_valor)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_formulario_produto_botao_salvar)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun deveSerCapazDePreencherOsCamposESalvar() {
+        // act
+        clicaNoFAB()
+
+        preencheCamposDoFormulario(
+            nome = "Banana",
+            descricao = "Prata",
+            valor = "6.99"
+        )
+
+        //Assert
+        clicaEmSalvar()
+
+        onView(withText("Banana")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun deveSerCapazDeEditarUmProduto() {
+
+        //Arrange
+        clicaNoFAB()
+
+        preencheCamposDoFormulario(
+            nome = "Banana nanica",
+            descricao = "da feira",
+            valor = "5.99"
+        )
+
+        clicaEmSalvar()
+
+        onView(withText("Banana nanica"))
+            .perform(click())
+
+        //Act
+        onView(withId(R.id.menu_detalhes_produto_editar))
+            .perform(click())
+
+        preencheCamposDoFormulario(
+            nome = "Banana prata",
+            descricao = "da vendinha",
+            valor = "3.99"
+        )
+
+        //Assert
+        clicaEmSalvar()
+
+        onView(withText("Banana prata"))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun clicaEmSalvar() {
+        onView(withId(R.id.activity_formulario_produto_botao_salvar))
+            .perform(click())
+    }
+
+    private fun clicaNoFAB() {
+        onView(withId(R.id.activity_lista_produtos_fab))
+            .perform(click())
+    }
+
+    private fun preencheCamposDoFormulario(
+        nome: String,
+        descricao: String,
+        valor: String
+    ) {
+        onView(withId(R.id.activity_formulario_produto_nome))
+            .perform(
+                replaceText(nome),
+                closeSoftKeyboard()
+            )
+        onView(withId(R.id.activity_formulario_produto_descricao))
+            .perform(
+                replaceText(descricao),
+                closeSoftKeyboard()
+            )
+        onView(withId(R.id.activity_formulario_produto_valor))
+            .perform(
+                replaceText(valor),
+                closeSoftKeyboard()
+            )
+    }
+}
---
+++ app/src/test/java/br/com/alura/orgs/ProdutoTests.kt
@@ -0,0 +1,118 @@
+package br.com.alura.orgs
+
+import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.model.Usuario
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeTrue
+import org.junit.Assert
+import org.junit.Test
+import java.math.BigDecimal
+
+class ProdutoTests {
+
+    @Test
+    fun `deve retornar verdadeiro quando o valor for valido`() {
+        //Arrange - Arrumar
+        val produtoValido = Produto(
+            nome = "Banana",
+            descricao = "Banana prata",
+            valor = BigDecimal("6.99")
+        )
+
+        //Act - Agir
+        val valorEhValido = produtoValido.valorEhValido
+
+        //Assert - Afirme
+        valorEhValido.shouldBeTrue()
+    }
+
+    @Test
+    fun `deve retornar falso quando o valor for maior que cem`() {
+        //Arrange
+        val produtoInvalido = Produto(
+            nome = "Carambola",
+            descricao = "Amarela",
+            valor = BigDecimal("105.99")
+        )
+
+        //Act
+        val valorEhValido = produtoInvalido.valorEhValido
+
+        //Assert
+        valorEhValido.shouldBeFalse()
+    }
+
+    @Test
+    fun `deve retornar falso quando o valor for menor ou igual a zero`() {
+        //Arrange
+        val produtoComValorIgualAZero = Produto(
+            nome = "Lichia",
+            descricao = "Doce",
+            valor = BigDecimal("0")
+        )
+        val produtoComValorMenorQueZero = Produto(
+            nome = "Uva",
+            descricao = "Thompson",
+            valor = BigDecimal("-10.99")
+        )
+
+        //Act
+        val valorIgualAZeroEhValido = produtoComValorIgualAZero.valorEhValido
+        val valorMenorQueZeroEhValido = produtoComValorMenorQueZero.valorEhValido
+
+        //Assert
+        valorIgualAZeroEhValido.shouldBeFalse()
+        valorMenorQueZeroEhValido.shouldBeFalse()
+    }
+}
+
+class TestaUsuario {
+
+    @Test
+    fun retornaVerdadeiroSeOsDadosDoUsuarioEstaoCertos() {
+        //Arrange
+        val usuarioValido = Usuario(
+            id = "usuario",
+            email = "usuario@valido.com",
+            senha = "senhaforte123"
+        )
+
+        //Act
+        val resultado = usuarioValido.ehValido()
+
+        //Assert
+        Assert.assertTrue(resultado)
+    }
+
+    @Test
+    fun retornaFalsoSeOEmailDoUsuarioEstiverErrado() {
+        //Arrange
+        val usuarioInvalido = Usuario(
+            id = "usuario",
+            email = "usuarioInvalido.com",
+            senha = "senhaforte123"
+        )
+
+        //Act
+        val resultado = usuarioInvalido.ehValido()
+
+        //Assert
+        Assert.assertFalse(resultado)
+    }
+
+    @Test
+    fun retornaFalsoSeASenhaDoUsuarioEstiverErradaComMenosDe6Caracteres() {
+        //Arrange
+        val usuarioInvalido = Usuario(
+            id = "usuario",
+            email = "usuario@valido.com",
+            senha = "fraca"
+        )
+
+        //Act
+        val resultado = usuarioInvalido.ehValido()
+
+        //Assert
+        Assert.assertFalse(resultado)
+    }
+}
