7a9fe82ea756a3825d53754a9c57deada6f58e6c
Lucas Almeida <101592386+lucasalmeida2@users.noreply.github.com>
2023-07-26 17:29:37 -0300

    Revert "Sonarcloud"

--- app/src/main/java/br/com/alura/orgs/database/converter/Converters.kt
+++ app/src/main/java/br/com/alura/orgs/database/converter/Converters.kt
@@ -16,17 +16,3 @@ class Converters {
     }
 
 }
-
-class Converters {
-
-    @TypeConverter
-    fun deDouble(valor: Double?) : BigDecimal {
-        return valor?.let { BigDecimal(valor.toString()) } ?: BigDecimal.ZERO
-    }
-
-    @TypeConverter
-    fun bigDecimalParaDouble(valor: BigDecimal?) : Double? {
-        return valor?.let { valor.toDouble() }
-    }
-
-}
